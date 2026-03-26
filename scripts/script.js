4b195895d59bc240a665c087ff8ef316815b9b84
Renato L. Vicente <renato_l_vicente@estudante.sesisenai.org.br>
2026-03-26 20:14:23 -0300

    style - melhoreando a aparencia da calculadora

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,27 @@
 function soma(a,b){
     const num1 = parseFloat(document.getElementById("numero1").value);//nesta linha ele força o numero que foi digitado ser um tipo de valor numérico
     const num2 = parseFloat(document.getElementById("numero2").value);
+
     return alert("Sua resposta é: "+ (num1 + num2));
 }
 function sub(a,b){
     const num1 = parseFloat(document.getElementById("numero1").value);
     const num2 = parseFloat(document.getElementById("numero2").value);
+
     return alert("Sua resposta é: " + (num1 - num2));
 }
 function multi(a,b){
     const num1 = parseFloat(document.getElementById("numero1").value);
     const num2 = parseFloat(document.getElementById("numero2").value);
+
     return alert("Sua resposta é: " + (num1 * num2));
 }
 function divi(a,b){
     const num1 = parseFloat(document.getElementById("numero1").value);
     const num2 = parseFloat(document.getElementById("numero2").value);
+    
     let di_zero = 0;    
+
     if(num1 != 0 && num2 != 0){
         di_zero = num1 / num2;
     }else{
