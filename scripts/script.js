93dd45f55b623f70f73608ff7cfa97533ee828d4
Renato L. Vicente <renato_l_vicente@estudante.sesisenai.org.br>
2026-03-26 20:15:36 -0300

    add -  explicações

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 function soma(a,b){
     const num1 = parseFloat(document.getElementById("numero1").value);//nesta linha ele força o numero que foi digitado ser um tipo de valor numérico
-    const num2 = parseFloat(document.getElementById("numero2").value);
+    const num2 = parseFloat(document.getElementById("numero2").value);//.value serve para pegar o valor que está dentro o input ao invez de pegar o input
 
     return alert("Sua resposta é: "+ (num1 + num2));
 }
@@ -19,10 +19,10 @@ function multi(a,b){
 function divi(a,b){
     const num1 = parseFloat(document.getElementById("numero1").value);
     const num2 = parseFloat(document.getElementById("numero2").value);
-    
+
     let di_zero = 0;    
 
-    if(num1 != 0 && num2 != 0){
+    if(num1 != 0 && num2 != 0){// esse if verifica se não é 0
         di_zero = num1 / num2;
     }else{
         di_zero = "Não é possivel fazer divisão por 0";
