e920d289f376379c6137e2135fd5e62dd8b3749e
Renato L. Vicente <renato_l_vicente@estudante.sesisenai.org.br>
2026-03-26 19:48:03 -0300

    feat  -  fazendo a calculadora funcionar

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,27 @@
 function soma(a,b){
-    let a = document.getElementById("numero1");
-    let b = document.getElementById("numero2");
-    return console.log(a + b);
+    const num1 = parseFloat(document.getElementById("numero1").value);//nesta linha ele força o numero que foi digitado ser um tipo de valor numérico
+    const num2 = parseFloat(document.getElementById("numero2").value);
+    return alert("Sua resposta é: "+ (num1 + num2));
 }
 function sub(a,b){
-    let a = document.getElementById("numero1");
-    let b = document.getElementById("numero2");
-    return console.log(a - b);
+    const num1 = parseFloat(document.getElementById("numero1").value);
+    const num2 = parseFloat(document.getElementById("numero2").value);
+    return alert("Sua resposta é: " + (num1 - num2));
 }
 function multi(a,b){
-    let a = document.getElementById("numero1");
-    let b = document.getElementById("numero2");
-    return console.log(a * b);
+    const num1 = parseFloat(document.getElementById("numero1").value);
+    const num2 = parseFloat(document.getElementById("numero2").value);
+    return alert("Sua resposta é: " + (num1 * num2));
 }
 function divi(a,b){
-    let a = document.getElementById("numero1");
-    let b = document.getElementById("numero2");
-    return console.log(a / b);
+    const num1 = parseFloat(document.getElementById("numero1").value);
+    const num2 = parseFloat(document.getElementById("numero2").value);
+    let di_zero = 0;    
+    if(num1 != 0 && num2 != 0){
+        di_zero = num1 / num2;
+    }else{
+        di_zero = "Não é possivel fazer divisão por 0";
+    }
+
+    return alert("Sua resposta é: " + di_zero);
 }
