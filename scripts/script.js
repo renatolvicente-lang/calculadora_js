b58a9b18be7db0e9b65ffcb34ae743819b975681
renatolvicente-lang <renato_l_vicente@estudante.sesisenai.org.br>
2026-03-27 12:21:53 -0300

    feat - aprendendo a usar addEventListeners()

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,32 @@
+
+const input_soma = document.getElementById("+").addEventListener("click", soma)//basicamente essa linha substitue a função do onclick().
+
 function soma(a,b){
     const num1 = parseFloat(document.getElementById("numero1").value);//nesta linha ele força o numero que foi digitado ser um tipo de valor numérico
     const num2 = parseFloat(document.getElementById("numero2").value);//.value serve para pegar o valor que está dentro o input ao invez de pegar o input
 
     return alert("Sua resposta é: "+ (num1 + num2));
 }
+
+const input_sub = document.getElementById("-").addEventListener("click", sub)
+
 function sub(a,b){
     const num1 = parseFloat(document.getElementById("numero1").value);
     const num2 = parseFloat(document.getElementById("numero2").value);
 
     return alert("Sua resposta é: " + (num1 - num2));
 }
+
+const input_multi = document.getElementById("*").addEventListener("click", multi)
+
 function multi(a,b){
     const num1 = parseFloat(document.getElementById("numero1").value);
     const num2 = parseFloat(document.getElementById("numero2").value);
 
     return alert("Sua resposta é: " + (num1 * num2));
 }
+const input = document.getElementById("/").addEventListener("click", divi)
+
 function divi(a,b){
     const num1 = parseFloat(document.getElementById("numero1").value);
     const num2 = parseFloat(document.getElementById("numero2").value);
