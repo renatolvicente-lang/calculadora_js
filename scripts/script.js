985efb89ef7a2c0de4d71f583eeaba84e2b8579a
Renato L. Vicente <renato_l_vicente@estudante.sesisenai.org.br>
2026-03-29 11:24:39 -0300

    feat - add funcionalidade de historico e algum comentarios para memorização

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,12 @@
-
-const input_soma = document.getElementById("+").addEventListener("click", soma)//basicamente essa linha substitue a função do onclick().
+// Funcionalidade de Operações 
+const input_soma = document.getElementById("+").addEventListener("click", soma);//basicamente essa linha substitue a função do onclick().
 
 function soma(a,b){
     const num1 = parseFloat(document.getElementById("numero1").value);//nesta linha ele força o numero que foi digitado ser um tipo de valor numérico
     const num2 = parseFloat(document.getElementById("numero2").value);//.value serve para pegar o valor que está dentro o input ao invez de pegar o input
     
     const res = num1 + num2; // aqui ele soma os dois numeros
+    historico.push(res);//aqui pega o resultado e guarda no array historico.   
     document.getElementById("resultado").value = res; //aqui ele exibe o resultado no output substituindo o valor dele pesa operação feitas
     
 }
@@ -15,8 +16,9 @@ const input_sub = document.getElementById("-").addEventListener("click", sub)
 function sub(a,b){
     const num1 = parseFloat(document.getElementById("numero1").value);
     const num2 = parseFloat(document.getElementById("numero2").value);
-
+    
     const res = num1 - num2;
+    historico.push(res);
     document.getElementById("resultado").value = res;
 }
 
@@ -25,8 +27,9 @@ const input_multi = document.getElementById("*").addEventListener("click", multi
 function multi(a,b){
     const num1 = parseFloat(document.getElementById("numero1").value);
     const num2 = parseFloat(document.getElementById("numero2").value);
-
+    
     const res = num1 * num2;
+    historico.push(res)
     document.getElementById("resultado").value = res;
 }
 const input = document.getElementById("/").addEventListener("click", divi)
@@ -34,14 +37,31 @@ const input = document.getElementById("/").addEventListener("click", divi)
 function divi(a,b){
     const num1 = parseFloat(document.getElementById("numero1").value);
     const num2 = parseFloat(document.getElementById("numero2").value);
-
+    
     let di_zero = 0;    
-
+    
     if(num1 != 0 && num2 != 0){// esse if verifica se não é 0
         di_zero = num1 / num2;
     }else{
         di_zero = "Não é possivel fazer divisão por 0";
     }
-
+    historico.push(di_zero);
     document.getElementById("resultado").value = di_zero;
+}
+
+//Funcionalidade de Histórico
+
+
+let historico = []; // array que irá armazenar o historico.
+
+function at_historico(){
+    let ul = document.getElementById("lista_hist"); // aqui puxa o elemento da lista com id "lista_hist" e joga na variavel "ul"
+    ul.inneHTML = ""; // aqui serve para limpar a lista antes de recriar e botar as operações
+    
+    historico.forEach(function(item){ // percorre o array para criar os elemento li novos
+        let li = document.createElement("li"); //cria os elementos li
+        li.textContent = "Resultado: " + item; // modifica o conteudo dos li
+        
+        ul.appendChild(li); // aqui pega as li e coloca dentro da ul
+    });
 }
